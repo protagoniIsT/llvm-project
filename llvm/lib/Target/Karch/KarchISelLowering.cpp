41a3ba56462285c5cf3831b3e55c6c9c35d0112f
vboxuser <gordienkokosta1@gmail.com>
2026-03-24 00:30:20 +0300

    Extend KarchTargetLowering with KarchRegisterInfo

--- llvm/lib/Target/Karch/KarchISelLowering.cpp
+++ llvm/lib/Target/Karch/KarchISelLowering.cpp
@@ -40,6 +40,28 @@ KarchTargetLowering::KarchTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   KARCH_DUMP_RED
   addRegisterClass(MVT::i32, &Karch::GPRRegClass);
+
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(Karch::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
 
 const char *KarchTargetLowering::getTargetNodeName(unsigned Opcode) const {
